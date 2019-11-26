6e5c23ac7af9bb166e75872607658544e623b878
Byurhan Beyzat <imbyurhan@gmail.com>
2019-11-26 18:54:17 +0200

    Initial setup 🚀

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  endOfLine: 'lf',
+  semi: false,
+  tabWidth: 2,
+  printWidth: 80,
+  singleQuote: true,
+  trailingComma: 'all',
+  jsxBracketSameLine: false,
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+<h1 align="center">
+  Referral Roulette
+</h1>
