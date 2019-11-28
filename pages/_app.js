44c9880f042e8f692737d82ce02f750b014dd503
Byurhan Beyzat <imbyurhan@gmail.com>
2019-11-28 21:18:44 +0200

    Added nprogress bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import App from 'next/app'
+import Router from 'next/router'
+import NProgress from 'nprogress'
 
 import Layout from '../components/layout'
 
 import '../styles/app.scss'
+
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 export default class MyApp extends App {
   render() {
