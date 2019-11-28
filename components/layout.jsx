a9209bbcda6bebce69ef62dea3e3c9af5f90ed5e
Byurhan Beyzat <imbyurhan@gmail.com>
2019-11-28 20:54:41 +0200

    Remove header...

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 
 import SEO from './seo'
-import Header from './header'
 
 const Layout = ({ title, children }) => (
   <>
     <SEO title={title} />
-    <Header />
-    <main role="main" style={{ paddingTop: '2.5rem' }}>
+    <main role="main" style={{ paddingTop: '10rem' }}>
       {children}
     </main>
   </>
