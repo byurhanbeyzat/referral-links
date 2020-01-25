6e033661c743b1e8d413457515378715ce67b2b2
Byurhan Beyzat <imbyurhan@gmail.com>
2020-01-25 18:45:00 +0200

    Add graphql schema and faunadb setup

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
+require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` })
 const withSass = require('@zeit/next-sass')
 
-module.exports = withSass({})
+module.exports = withSass({
+  env: {
+    ENV: process.env.NODE_ENV,
+    DB_SECRET_KEY: process.env.DB_SECRET_KEY,
+    GRAPHQL_ENDPOINT: process.env.GRAPHQL_ENDPOINT,
+  },
+})
